2a0d9e5cf0a797e01aa1029e1d59bba13cd1de48
Eric Yang Jia Wei <eric.yang@temasek.nus.edu.sg>
2020-12-11 16:26:47 +0800

    Add routes to other pages

--- src/Components/dashboard.js
+++ src/Components/dashboard.js
@@ -1,65 +1,65 @@
 // import logo from './logo.svg';
 // import './App.css';
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 // this is the example response
 
 // [{"accountName":"Current Account","availableBal":994.32,"accountNumber":52644815,"linked":false},{"accountName":"Saving Account","availableBal":530.35,"accountNumber":34637629,"linked":true},{"accountName":"Multiplier Account","availableBal":533.35,"accountNumber":93035304,"linked":false}]
 
 class Dashboard extends Component {
+  state = {
+    accountName: [],
+    accountNumber: [],
+    availableBal: [],
+    linked: [],
+  };
 
-    state = {
-        accountName: [],
-        accountNumber: [],
-        availableBal: [],
-        linked: [], 
-
-    }
-
-
-
-    render() {
-        return (
-          <div className="Dashboard">
-          <header className="Dashboard-header">
-          <p>
-          Welcome Trey Swaniawski to your e-wallet 
-          </p>
-            <div class="Dashbody-body">
-              <h5 class="account-balance">This is your available account balance: $994.32</h5>
-            </div>
-            </header>
+  render() {
+    return (
+      <div className="Dashboard">
+        <header className="Dashboard-header">
+          <p>Welcome Trey Swaniawski to your e-wallet</p>
+          <div class="Dashbody-body">
+            <h5 class="account-balance">
+              This is your available account balance: $994.32
+            </h5>
           </div>
-        );
-      }
-    
-// }
-// submitHandler(event) {
-//     event.preventDefault();
-//     let data = {
-//       username: this.state.username,
-//       password: this.state.password,
-//     };
-//     fetch(
-//       "https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/login",
-//       {
-//         method: "POST",
-//         body: JSON.stringify(data),
-//         headers: {
-//           "x-api-key": "AvznKwHoRPaClLwHbinGV8RezzoVYl5f7aogWGl3",
-//         },
-//       }
-//     ).then((res) =>{ 
-//         console.log(res)
-//     });
-    /*axios.post("/login", data)
+        </header>
+        <Link to="/trans_history">Transaction History</Link>
+        <br/>
+        <Link to="/trans_form">Transaction Form</Link>
+      </div>
+    );
+  }
+
+  // }
+  // submitHandler(event) {
+  //     event.preventDefault();
+  //     let data = {
+  //       username: this.state.username,
+  //       password: this.state.password,
+  //     };
+  //     fetch(
+  //       "https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/login",
+  //       {
+  //         method: "POST",
+  //         body: JSON.stringify(data),
+  //         headers: {
+  //           "x-api-key": "AvznKwHoRPaClLwHbinGV8RezzoVYl5f7aogWGl3",
+  //         },
+  //       }
+  //     ).then((res) =>{
+  //         console.log(res)
+  //     });
+  /*axios.post("/login", data)
     .then((res) => {
       console.log(res);
     })
     .catch(err => {
         console.log(err)
     });*/
-  }
+}
 
 // function Dashboard() {
 //   return (
