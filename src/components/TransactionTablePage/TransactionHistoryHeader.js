b7b06c0ac0be48d8475c818e574b49a607bdb29f
Eric Yang Jia Wei <eric.yang@temasek.nus.edu.sg>
2020-12-11 12:00:38 +0800

    Add simple transaction page header

---
+++ src/components/TransactionTablePage/TransactionHistoryHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let user = "myName";
+
+const TransactionHistoryHeader = () => {
+  return (
+    <div>Welcome ${user} to your transaction history</div>
+  );
+};
+
+export default TransactionHistoryHeader;
